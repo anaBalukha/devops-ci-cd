f18bdb74ffb56a8ff8e44da79d1f799b1ebc074e
ana <balukhashviliana@gmail.com>
2026-04-24 19:11:38 +0400

    Fix test for new UI

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,5 +3,5 @@ const app = require("../index");
 
 test("GET / returns Hello CI/CD!", async () => {
   const res = await request(app).get("/");
-  expect(res.text).toBe("Hello CI/CD!");
+  expect(res.text).toContain("Hello, I'm Anna Balukhashvili");
 });
